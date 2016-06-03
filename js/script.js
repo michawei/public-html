62bbf0b024de75359aeeb3455cb5ae8a0404378d
michawei <freda0330@yahoo.com.tw>
2016-06-04 02:20:18 +0800

    animate

--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-var lastPage = "content_1";
-
-var changePage = function(nextPage){
-	console.log(nextPage);
-	$("#" + lastPage).css("display", "none");
-	$("#" + nextPage).css("display", "block");
-	lastPage = nextPage;
-}
